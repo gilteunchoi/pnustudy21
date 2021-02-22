8e0513484282bcb0c65edc1838f06a8cc3fa1a97
SungheeJin1 <78209145+SungheeJin1@users.noreply.github.com>
2021-02-22 20:16:06 +0900

    Add files via upload

---
+++ SungheeJin1/demo8.js
@@ -0,0 +1,50 @@
+while(true){
+	//여기에 명령을 입력하면 무한대로 실행
+}
+
+var i = 5;
+while(i>=0/*i의 값이 0 이상일 때만 실행*/){
+	document.write('a '+i+'<br />');
+	i-- //텍스트 출력할 때마다 i의 값이 1씩 감소
+}
+
+var i = 5;
+while(i>=0/*i의 값이 0 이상일 때만 실행*/){
+	document.write('a '+i+'<br />');
+	--i //i = 0에서 1을 뺀 후 텍스트 출력, 결과는 같다
+
+
+
+
+i = 0;
+0
+console.log(i++);
+VM1244:1 0
+undefined
+console.log(i++);
+VM1249:1 1
+undefined
+console.log(i++);
+VM1254:1 2
+undefined
+console.log(i++);
+VM1259:1 3
+undefined
+console.log(i++);
+VM1264:1 4
+undefined
+
+i = 0;
+0
+console.log(++i);
+VM1336:1 1
+undefined
+console.log(++i);
+VM1341:1 2
+undefined
+console.log(++i);
+VM1345:1 3
+undefined
+console.log(++i);
+VM1349:1 4
+undefined
